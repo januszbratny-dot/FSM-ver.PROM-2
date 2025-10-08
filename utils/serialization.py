14229f5b38c4f97dfcb6fc4674bca6f6af4db38f
januszbratny-dot <janusz.bratny@gmail.com>
2025-10-08 17:02:21 +0200

    Update serialization.py

--- utils/serialization.py
+++ utils/serialization.py
@@ -1,13 +1,15 @@
 from datetime import datetime, time
 
-def datetime_to_iso(dt: datetime) -> str:
+def datetime_to_iso(dt):
     return dt.isoformat() if dt else None
 
-def time_to_iso(t: time) -> str:
-    return t.isoformat()
+def time_to_iso(t):
+    return t.isoformat() if t else None
 
-def parse_datetime_iso(s: str) -> datetime:
+def parse_datetime_iso(s):
+    if not s:
+        return None
     if s.endswith("Z"):
         s = s[:-1] + "+00:00"
     dt = datetime.fromisoformat(s)
-    return dt.astimezone().replace(tzinfo=None) if dt.tzinfo else dt
+    return dt.replace(tzinfo=None)
