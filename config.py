4b0b7aa137477a193f0e19e169124b837eee16dc
januszbratny-dot <janusz.bratny@gmail.com>
2025-10-08 17:28:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 from datetime import time
-import logging
 
 STORAGE_FILENAME = "schedules.json"
 SEARCH_STEP_MINUTES = 15
 DEFAULT_WORK_START = time(8, 0)
 DEFAULT_WORK_END = time(16, 0)
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
-logger = logging.getLogger("scheduler")
+# Przykładowe preferowane sloty
+PREFERRED_SLOTS = {
+    "8:00-11:00": (time(8, 0), time(11, 0)),
+    "11:00-14:00": (time(11, 0), time(14, 0)),
+    "14:00-17:00": (time(14, 0), time(17, 0)),
+    "17:00-20:00": (time(17, 0), time(20, 0)),
+}
