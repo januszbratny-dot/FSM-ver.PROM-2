0d6efa22098fbe4075abca3455f9125e0060ecf7
januszbratny-dot <janusz.bratny@gmail.com>
2025-10-08 16:59:08 +0200

    Update config.py
    Konfiguracja aplikacji i logowania.

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ SEARCH_STEP_MINUTES = 15
 DEFAULT_WORK_START = time(8, 0)
 DEFAULT_WORK_END = time(16, 0)
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 logger = logging.getLogger("scheduler")
